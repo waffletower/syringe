3bb854da83bc98e4a0de6c59086d1cf88de6ae48
Christopher Penrose <bonus@leisuresonic.com>
2017-08-16 23:51:42 -0700

    upped to '0.1.1'

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject waffletower/syringe "0.1.0"
+(defproject waffletower/syringe "0.1.1"
   :description "Clojure code suitable for dev profile injection"
   :url "https://github.com/waffletower/syringe"
   :license {:name "Eclipse Public License"
