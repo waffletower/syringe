6a8933a6e8e7e5ba9a6d8da4af833f94aa4463d3
Christopher Penrose <waffletower@users.noreply.github.com>
2021-12-29 15:29:46 -0800

    updated dependencies. added skeletal deps.edn.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject waffletower/syringe "0.4.0"
+(defproject waffletower/syringe "0.5.0"
   :description "Clojure code suitable for dev profile injection"
   :url "https://github.com/waffletower/syringe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [mvxcvi/puget "1.3.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [mvxcvi/puget "1.3.2"]
                  [environ "1.2.0"]]
   :deploy-repositories [["clojars-https" {:url "https://clojars.org/repo"
                                           :username :env/clojars_user
