473ca623302841527db3b0fc052633a73fd474ea
Christopher Penrose <waffletower@users.noreply.github.com>
2023-11-03 20:27:33 -0700

    updated dependencies.  removed 'environ', added java.classpath and tools.namespace.

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [mvxcvi/puget "1.3.2"]
-                 [environ "1.2.0"]]
+                 [org.clojure/java.classpath "1.0.0"]
+                 [org.clojure/tools.namespace "1.4.4"]
+                 [mvxcvi/puget "1.3.2"]]
   :deploy-repositories [["clojars-https" {:url "https://clojars.org/repo"
                                           :username :env/clojars_user
                                           :password :env/clojars_password}]])
